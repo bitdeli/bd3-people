63ee52512ba55dc0121202657fdf5939bb64b3b9
Ville Tuulos <team+foobar@bitdeli.com>
2013-02-20 05:02:42 +0000

    use Users widget instead of Table

--- insight.py
+++ insight.py
@@ -1,18 +1,18 @@
 from bitdeli.insight import insight
-from bitdeli.widgets import Table
+from bitdeli.widgets import Users
 from discodb.query import Q, Literal, Clause
 from itertools import islice
 import json
 
 def rows(model):
     for uid, values in islice(model.items(), 50):
-        rows = {' uid': uid}
+        rows = {'uid': uid}
         rows.update(tuple(json.loads(value)) for value in values)
         yield rows
 
 @insight
 def view(model, params):
-    return [Table(label='people',
+    return [Users(label='people',
                   size=(12, "auto"),
                   data=list(rows(model)))]
                   
