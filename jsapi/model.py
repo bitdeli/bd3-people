f32f1cfafa34b1a65e128beac7fa37e688b7070a
Ville Tuulos <ville@bitdeli.com>
2013-03-28 04:04:53 +0000

    add ip to profile

--- jsapi/model.py
+++ jsapi/model.py
@@ -3,24 +3,6 @@ from itertools import islice
 from datetime import datetime
 from bitdeli.model import model, segment_model
 
-def newest(attr, top=1):
-    items = ((iter(hours).next()[0], value)
-             for value, hours in attr.iteritems())
-    if top == 1:
-        return max(items)[1]
-    else:
-        return [(value, datetime.utcfromtimestamp(hour * 3600).isoformat())
-                for hour, value in sorted(items)][-top:]
-
-def attributes(properties):
-    for key, prop in properties.iteritems():
-        if key[0] == '$' or key[:3] == 'mp_':
-            yield key, newest(prop)
-        elif 'email' in key:
-            maybe_email = newest(prop)
-            if '@' in maybe_email:
-                yield key, maybe_email
-                
 @model
 def build(profiles):
     for profile in profiles:
@@ -30,6 +12,7 @@ def build(profiles):
             events.append((event.pop('$event_name'), tstamp))
             if first:
                 first = False
+                yield profile.uid, json.dumps(('ip', ip))
                 for key, val in event.iteritems():
                     yield profile.uid, json.dumps((key, val))
         yield profile.uid, json.dumps(('events', events))
